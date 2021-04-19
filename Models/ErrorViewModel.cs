81bc4f31437e76822e63d4ff4e00dfd46bace9cf
Kanswata <geezyk695@gmail.com>
2021-04-19 22:59:11 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KanswataJR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
